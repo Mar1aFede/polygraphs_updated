89d362f9d0cc9e7e72fc43cc363b549de8d09609
Mar1aFede <99097614+Mar1aFede@users.noreply.github.com>
2024-10-15 02:25:54 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ import setuptools
 VERSION = "1"
 
 setuptools.setup(
-    name="polygraphs",
+    name="polygraphs_updated",
     version=VERSION,
     description="PolyGraphs",
     long_description="",
     author="Alexandros Koliousis",
     author_email="ak@akoliousis.com",
-    url="https://github.com/alexandroskoliousis/polygraphs",
+    url="https://github.com/Mar1aFede/polygraphs_updated",
     license="MIT",
     keywords="test test",
     packages=setuptools.find_packages(exclude=("tests",)),

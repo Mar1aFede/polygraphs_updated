afb629c747cd7fa8924af69c75f969391af50a87
Mar1aFede <99097614+Mar1aFede@users.noreply.github.com>
2024-10-15 11:58:13 +0100

    Update simulation_processor.py

--- polygraphs_updated/analysis/simulation_processor.py
+++ polygraphs_updated/analysis/simulation_processor.py
@@ -131,10 +131,7 @@ class SimulationProcessor:
 
         # Resolve path to config file using pathlib
         config_path = subfolder_path / "configuration.json"
-        #print(f"Checking for config file at: {config_path}")  
         # If no configuration file is found, skip processing this subfolder
-        #if not config_path.exists():
-           #print(f"No configuration file found at {config_path}.")
         return 
        
         # Load the configuration JSON file
